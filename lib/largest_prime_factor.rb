9b3f79b5f2723ce39c58b863e5433495c5948b9d
wkdewey <wkdewey@gmail.com>
2020-12-10 15:19:09 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,32 @@
 # Enter your procedural solution here!
+# @primes = [2, 3]
+
+def is_prime?(number)
+  puts "testing " + number.to_s
+  prime = true
+  if !(number == 2 || number == 3)
+    squareRoot = Math.sqrt(number).floor
+    squareRoot.downto(2).each { |i|
+      if ((number % i) == 0) && is_prime?(i)
+        prime = false
+      end
+    }
+  end
+  prime
+end
+
+def largest_prime_factor(number)
+  squareRoot = Math.sqrt(number).floor
+  squareRoot.downto(2).each { |i|
+    if ((number % i) == 0) && is_prime?(i)
+      return i
+    end
+  }
+  return number
+end
+# @primes = [2, 3]
+# puts @primes
+# def put_primes
+#   puts @primes
+# end
+# put_primes()
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  @@primes = [2, 3]
+  def initialize(number)
+    @number = number
+  end
+
+  def self.is_prime?(number)
+    prime = true
+    if !(@@primes.include?(number))
+      squareRoot = Math.sqrt(number).floor
+      for i in 2..squareRoot
+        if ((number % i) == 0) && is_prime?(i)
+          prime = false
+        end
+      end
+    end
+    if prime == true && !(@@primes.include?(number))
+      @@primes << number
+    end
+    prime
+  end
+
+  def number
+    squareRoot = Math.sqrt(@number).floor
+    squareRoot.downto(2).each { |i|
+      if ((@number % i) == 0) && LargestPrimeFactor.is_prime?(i)
+        return i
+      end
+    }
+    return @number
+  end
+end
